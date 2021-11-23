f477f13f20172ceececcb1eb71c0302c754e1828
Richard Dixon <rvictordixon@gmail.com>
2021-11-22 23:42:00 -0500

    still working on substring solution.

--- leetcode/strings/repeated_substringUF.js
+++ leetcode/strings/repeated_substringUF.js
@@ -9,32 +9,62 @@
 // Output: true
 // Explanation: It is the substring "abc" four times or the substring "abcabc" twice
 
+//1 <= s.length <= 104
+
 let repeatedSubstringPattern = function(s) {
     
     let subStr = ""
-
+    
     for(let i = 0; i < s.length; i++){
-        while(subStr.length < s.length){
-            subStr += s[i]
+        subStr += s[i]
+        let subFirstIndex = 0
+        let subEndIndex = subStr.length
+        
+        while(subEndIndex <= s.length - 1){
+            let matchString = s.substring(subFirstIndex, subEndIndex)
+            //console.log(matchString, subStr)
+            if(subStr !== matchString){
+                break
+            }
+            else{
+                subFirstIndex += subStr.length 
+                subEndIndex += subStr.length
+                if(subEndIndex === s.length){
+                    if(subStr === matchString){
+                        return true
+                    }
+                    else{
+                        break
+                    }
+                }
+                
+            }
+
         }
-        if(s === subStr){
-            return true
-            
-        }
-        else{
-            subStr = ""
-        }
+       
         
     }
     
-    return (subStr === s) ? true : false
+    return false
         
     
 };
 
 
-console.log(repeatedSubstringPattern("aba"))
+console.log(repeatedSubstringPattern("abcabcabcabc"))
 // iterate over string parameter
     // use the substring method 
     // declare a variable with substring 
     // start index , end index
+
+
+    // while(subStr.length < s.length){
+    //     subStr += s[i]
+    // }
+    // if(s === subStr){
+    //     return true
+        
+    // }
+    // else{
+    //     subStr = ""
+    // }
